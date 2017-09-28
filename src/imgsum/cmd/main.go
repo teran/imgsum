c2794b2621b63105dfa46d39279f2c04aebebe58
Igor Shishkin <me@teran.ru>
2017-09-28 15:21:47 +0300

    Check function added + gofmt
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- src/imgsum/cmd/main.go
+++ src/imgsum/cmd/main.go
@@ -1,66 +1,104 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "image"
-  _ "image/gif"
-  _ "image/jpeg"
-  _ "image/png"
-  "os"
-  "regexp"
-  "strconv"
+	"bufio"
+	"flag"
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
-  "github.com/jteeuwen/imghash"
-  "github.com/nf/cr2"
+	"github.com/jteeuwen/imghash"
+	"github.com/nf/cr2"
 )
 
 var (
-  re_canon = regexp.MustCompile(".cr(2|w)$")
+	re_canon = regexp.MustCompile(".cr(2|w)$")
 )
 
-func calculate(file string) (error) {
-  var img image.Image
-  img, err := getImage(file)
-  if err != nil {
-    fmt.Fprintln(os.Stderr, file, err.Error())
-    return err
-  }
+func calculate(file string) error {
+	var img image.Image
+	img, err := getImage(file)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, file, err.Error())
+		return err
+	}
 
-  h, err := hash(img)
-  if err != nil {
-    fmt.Fprintln(os.Stderr, file, err.Error())
-    return err
-  }
+	h, err := hash(img)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, file, err.Error())
+		return err
+	}
 
-  fmt.Printf("%v  %v\n", strconv.Itoa(int(h)), file)
-  return nil
+	fmt.Printf("%v  %v\n", strconv.Itoa(int(h)), file)
+	return nil
 }
 
-func checkFiles(checksum_file string) {}
+func checkFiles(checksumFile string) error {
+	fp, err := os.Open(checksumFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, checksumFile, err.Error())
+		return err
+	}
+	defer fp.Close()
+
+	r := bufio.NewReader(fp)
+	line, err := r.ReadString(10)
+	for err != io.EOF {
+		fields := strings.Fields(line)
+		if len(fields) == 2 {
+			img, err := getImage(fields[1])
+			if err != nil {
+				fmt.Fprintln(os.Stderr, fields[1], err.Error())
+				return err
+			}
+
+			h, err := hash(img)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, fields[1], err.Error())
+				return err
+			}
+
+			if fields[0] == strconv.Itoa(int(h)) {
+				fmt.Printf("%v: OK\n", fields[1])
+			} else {
+				fmt.Printf("%v: FAILED\n", fields[1])
+			}
+		}
+
+		line, err = r.ReadString(10)
+	}
+	return nil
+}
 
 func getImage(filename string) (image.Image, error) {
-  var img image.Image
-  var err error
-  var fp *os.File
+	var img image.Image
+	var err error
+	var fp *os.File
 
-  fp, err = os.Open(filename)
+	fp, err = os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
-  defer fp.Close()
+	defer fp.Close()
 
-  if re_canon.Match([]byte(filename)) {
-    img, err = cr2.Decode(fp)
-  } else {
-    img, _, err = image.Decode(fp)
-  }
+	if re_canon.Match([]byte(filename)) {
+		img, err = cr2.Decode(fp)
+	} else {
+		img, _, err = image.Decode(fp)
+	}
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  return img, nil
+	return img, nil
 }
 
 func hash(img image.Image) (uint64, error) {
@@ -68,18 +106,28 @@ func hash(img image.Image) (uint64, error) {
 }
 
 func main() {
-  flag.Usage = func() {
-    fmt.Printf("Usage: %s [OPTION]... [FILE]...\n", os.Args[0])
-    fmt.Printf("Print or check image Average hashes\n")
-  }
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [OPTION]... [FILE]...\n", os.Args[0])
+		fmt.Printf("Print or check image Average hashes\n")
+		fmt.Printf("  -check\n")
+		fmt.Printf("    read average hashes from the FILEs and check them\n")
+	}
 
-  flag.Parse()
-  if flag.NArg() < 1 {
-    flag.Usage()
-    os.Exit(1)
-  }
+	check := flag.Bool("check", false, "")
 
-  for file := range flag.Args() {
-    calculate(flag.Arg(file))
-  }
+	flag.Parse()
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *check == true {
+		for file := range flag.Args() {
+			checkFiles(flag.Arg(file))
+		}
+	} else {
+		for file := range flag.Args() {
+			calculate(flag.Arg(file))
+		}
+	}
 }
